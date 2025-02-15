e873c39c1f797457375fcf2ce77eea77d9d1bdc7
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2025-02-15 18:18:02 -0500

    Indexacion para motores de búsqueda

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://estuscode.com',
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
