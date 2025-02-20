d4327b8e8b476957b3d01212978b0ff23c4e0e2d
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2025-02-20 15:47:40 -0500

    update site URL to include 'www' for consistency in meta tags and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://estuscode.com',
+  site: 'https://www.estuscode.com',
   integrations: [
     tailwind(),
     sitemap()
