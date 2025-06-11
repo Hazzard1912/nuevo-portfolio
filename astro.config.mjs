6aca06a7368197f3740212c6a06949b78ef9e2de
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2025-06-10 22:45:40 -0500

    feat: actualizar configuraciones de Astro, optimizar imágenes y mejorar atributos de carga en componentes y páginas

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,17 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nexen.com.co',
+
   output: 'server',
   trailingSlash: 'always',
+  prefetch: {
+    defaultStrategy: 'hover',
+  },
   adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+
   }),
   integrations: [
     tailwind(),
